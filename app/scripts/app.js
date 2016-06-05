6f7e67ed45d619828072ff229d7efced6ba97f25
Quark Li <quarkli@gmail.com>
2016-06-05 09:39:59 +0800

    fixed backdrop block issue when page back is fired during a paper-dialog opened

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -54,6 +54,10 @@ subject to an additional IP rights grant found at http://polymer.github.io/PATEN
     }
   });
 
+  window.addEventListener('popstate', function() {
+    $('paper-dialog').prop('opened', false);
+  });
+
   // Main area's paper-scroll-header-panel custom condensing transformation of
   // the appName in the middle-container and the bottom title in the bottom-container.
   // The appName is moved to top and shrunk on condensing. The bottom sub title
